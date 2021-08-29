c4872702ed7f32d8638c376861428ce865d783e3
onodera <cxiaoyesi745@gmail.com>
2021-08-29 20:41:38 +0900

    initial commit

---
+++ My-janken2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-janken2
+//
+//  Created by Jun on 2021/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
