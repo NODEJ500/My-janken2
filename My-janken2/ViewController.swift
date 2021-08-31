a4ad6fd7f02fd1c4225cb8d852c42d5e21998c1c
onodera <cxiaoyesi745@gmail.com>
2021-08-31 22:30:57 +0900

    3rd Commit

--- My-janken2/ViewController.swift
+++ My-janken2/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var answerNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        answerNumber = Int.random(in: 0..<3)
+        
+        if answerNumber == 0{
+            //グー
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+            
+        }else if answerNumber == 1{
+            //チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        
+        }else if answerNumber == 2{
+            //パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+            
+    }
+  }
 }
-
