87fd69454ad3b3cf5de6cd9312f86c7c91fd654c
onodera <cxiaoyesi745@gmail.com>
2021-09-02 22:26:16 +0900

    last Commit

--- My-janken2/ViewController.swift
+++ My-janken2/ViewController.swift
@@ -14,18 +14,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBOutlet var answerImageView: UIImageView!
-    
     @IBOutlet weak var answerLabel: UILabel!
-    
     @IBAction func shuffleAction(_ sender: Any) {
         
-        answerNumber = Int.random(in: 0..<3)
         
-        if answerNumber == 0{
+        //新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
+        
+        //ランダムな結果を出すが、前回の結果と異なる場合のみ採用
+        //repeatは繰り返しを意味する
+        repeat{
+            
+            //0,1,2の数値をランダムに算出（乱数）
+            newAnswerNumber = Int.random(in: 0..<3)
+            
+            //前回と同じ結果のときは、再度、ランダムに数値を出す
+            //異なる結果のときは、repeatを抜ける
+        }while answerNumber == newAnswerNumber
+        
+        //新しいじゃんけんの結果を格納
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
             //グー
             answerLabel.text = "グー"
             answerImageView.image = UIImage(named: "gu")
